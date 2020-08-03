3108a2f6d40cdbcfbe6ac84fca7b1b1b9b22be78
Asim <ajbp68@mail.umsl.edu>
2020-08-03 13:41:45 -0500

    created log file, also added nodemon for dev

--- chaseandco/app.js
+++ chaseandco/app.js
@@ -2,9 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-// const hostname = '127.0.0.1';
-// const port = 3000;
+var morgan = require('morgan')
+var fs = require('fs')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,7 +14,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, '/logs/access.log'), { flags: 'a' })
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
